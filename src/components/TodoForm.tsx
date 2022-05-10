eb9f316a7000d32b9d14f527dc5feb2593fad36f
podsolnuh69 <kkoiku3@gmail.com>
2022-05-10 09:53:04 +0300

    add hook to src/components/TodoForm.tsx

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
 
 const TodoForm: React.FC = () => {
+    const ref = useRef<HTMLInputElement>(null);
+
+    const keyPressHandler = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            console.log(ref.current!.value)
+            ref.current!.value = '';
+        }
+    }
+
     return <div className="input-field todoform-input">
-        <input type="text" id='title' placeholder='Купить презервативов'/>
+        <input
+            ref={ref}
+            type="text"
+            id='title'
+            placeholder='Купить презервативов'
+            onKeyPress={keyPressHandler}/>
         <label htmlFor="title" className='active'>Введите название дела:</label>
     </div>
 }
